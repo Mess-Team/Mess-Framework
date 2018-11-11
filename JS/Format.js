f5b946e76d88b3fc5355a381589ff1c0fa9e58f9
Edward Gough <41752815+MrGough@users.noreply.github.com>
2018-11-11 14:01:35 +0000

    Moved Date Functions to Format File
    I've moved the Date functions that were originally in the Validation file into the format file as this is more fitting for them. Also tided it up a little.

--- JS/Format.js
+++ JS/Format.js
@@ -1,1 +1,32 @@
-// FORMAT JS
+// GET CURRENT DATE AND FORMAT
+function getDate()
+{
+    // SET CURRENT DATE
+    var dateObject = new Date();
+    var FetchedDate = FormatDate(dateObject);
+
+    return FetchedDate;
+}
+
+// FORMATED ANY PASSED DATE
+function FormatDate(PassedDate)
+{
+    // SET DATE VARIABLES
+    var dateObject = new Date(PassedDate);
+    var Month = dateObject.getUTCMonth() + 1;
+    var Day = dateObject.getUTCDate();
+    var Year = dateObject.getUTCFullYear();
+    var Full = Month + "/" + Day + "/" + Year;
+    var Seconds = dateObject.getTime() / 1000;
+    
+    // BUILD DATE ARRAY
+    var FormatedDate = {
+        'F': Full,
+        'S': Seconds,
+        'Y': Year,
+        'D': Day,
+        'M': Month
+    };
+    
+    return FormatedDate;
+}
