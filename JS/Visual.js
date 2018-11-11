1eb3af17b3e7dce1690a2cc42dcdad8428f0fbbf
Edward Gough <41752815+MrGough@users.noreply.github.com>
2018-11-11 14:03:38 +0000

    Laying the ground works for Visual.js
    Starting to lay some of the ground works for the Visual.js file. I'm not sure of the direction I'm going to take with the visual actions just yet.

--- JS/Visual.js
+++ JS/Visual.js
@@ -1,1 +1,10 @@
-// VISUAL JS
+// DATA VISUAL > HIDDEN
+$('[data-visual="hidden"]').each(function(){
+    $(this).css('display', 'none');
+});
+
+// DATA VISUAL > LABEL
+$('[data-lablel]').each(function(){
+    // BUILD LABEL HERE !!!!!!!!
+});
+
